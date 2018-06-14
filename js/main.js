136ac2f98141471049229c1c92950ed45da10d97
a-soliman <ahmed.soliman@programmer.net>
2018-06-13 20:48:30 -0700

    calling the once load functions

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const taskList  = document.querySelector('ul.collection');
 const clearBtn  = document.querySelector('.clear-tasks');
 const filter    = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// LOAD TASK FROM LOCALSTORAGE
+loadTasksFromLocalStorage();
+
+// LOAD ALL EVENT LISTENERS
+loadEventListener();
