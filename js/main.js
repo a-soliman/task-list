2b8f1abcfeae9fd40b3579d8482dd346ef425f13
a-soliman <ahmed.soliman@programmer.net>
2018-06-13 20:47:35 -0700

    defiend the UI vars

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// DEFINE UI VARS
+const form      = document.querySelector('form');
+const taskList  = document.querySelector('ul.collection');
+const clearBtn  = document.querySelector('.clear-tasks');
+const filter    = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
